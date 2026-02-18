3de74aa40ddd747b865006e44713885e910c4b1d
renner <renner0@posteo.de>
2026-02-19 00:01:24 +0100

    fix: make it actually work for new user accounts
    still using the default kde panel when you make a new default panel

--- system_files/shared/usr/share/plasma/look-and-feel/dev.getaurora.aurora.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
+++ system_files/shared/usr/share/plasma/look-and-feel/dev.getaurora.aurora.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -1,23 +1,25 @@
+loadTemplate("org.kde.plasma.desktop.defaultPanel")
+
 var allPanels = panels();
 
 for (var i = 0; i < allPanels.length; ++i) {
-    var widgets = allPanels[i].widgets();
+    const panel = allPanels[i];
+    var widgets = panel.widgets();
+
     for (var j = 0; j < widgets.length; ++j) {
         var widget = widgets[j];
 
-        if (widget.type === "org.kde.plasma.taskmanager" || widget.type === "org.kde.plasma.icontasks") {
+        if (widget.type === "org.kde.plasma.icontasks") {
+            widget.currentConfigGroup = ["General"];
 
-            widget.currentConfigGroup = new Array("General");
-
-            var newLaunchers = [
+            widget.writeConfig("launchers", [
                 "preferred://browser",
                 "applications:io.github.kolunmi.Bazaar.desktop",
                 "applications:org.gnome.Ptyxis.desktop",
                 "preferred://filemanager"
-            ];
-            
-            widget.writeConfig("launchers", newLaunchers);
+            ]);
             widget.reloadConfig();
         }
     }
 }
+
