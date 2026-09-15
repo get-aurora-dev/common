a9fd1f340b5c8ca4d3985bc0ff4b2cba6bfe59bf
renner <renner0@posteo.de>
2026-09-15 12:55:44 +0200

    fix: actually set middle mouse paste false in firefox

--- system_files/shared/usr/share/ublue-os/firefox-config/01-aurora-global.js
+++ system_files/shared/usr/share/ublue-os/firefox-config/01-aurora-global.js
@@ -3,4 +3,4 @@ pref("gfx.webrender.all", true);
 pref("media.hardware-video-decoding.force-enabled", true);
 // Hopefully upstream does this at some point :)
 // https://phabricator.services.mozilla.com/D277804
-pref("middlemouse.paste", true;
+pref("middlemouse.paste", false;
